0d2f89481e0362096575b74603bba4d6131ec664
jiaoyuqing147 <310945033@qq.com>
2025-07-14 23:28:48 +0800

    融合了MMLA和FASFFHead

--- run_all_train_scipts.py
+++ run_all_train_scipts.py
@@ -4,8 +4,10 @@ scripts = [
     "train_tt100k_pro_myxlab.py",
     "train_tt100k_pro_myxlab1.py",
     "train_tt100k_pro_myxlab2.py",
+    "train_tt100k_pro_myxlab21.py",
     "train_tt100k_pro_myxlab3.py",
     "train_tt100k_pro_myxlab4.py"
+    "train_tt100k_pro_myxlab5.py"
 ]
 
 for i, script in enumerate(scripts):
