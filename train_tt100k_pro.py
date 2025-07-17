6e6eff27110cc7f7df2c3adf8d20331107efaa1d
JIAOGE（本地账户） <310945033@qq.com>
2025-07-17 22:13:03 +0800

    更新了SmallObjectLDConv与LDConv的GLFOPS打印

--- train_tt100k_pro.py
+++ train_tt100k_pro.py
@@ -5,6 +5,8 @@ from ultralytics import YOLO
 import random
 import numpy as np
 import torch
+from ultralytics.utils.torch_utils import model_info#给自定义卷积模块增加打印GFLOPS的功能，需要这个导入
+
 # ✅ 设置随机种子（方法二：手动控制）
 def set_seed(seed=42):
     random.seed(seed)
@@ -18,7 +20,7 @@ if __name__ == '__main__':
     set_seed(42)
     #model = YOLO(r'ultralytics/cfg/models/11/yolo11-P234-FASFFHead_Jack.yaml')#使用这个结构,用大的版本就改字母yolo11l-P2-FASFFHead.yaml# 续训yaml文件的地方改为lats.pt的地址,需要注意的是如果你设置训练200轮次模型训练了200轮次是没有办法进行续训的.
     model = YOLO(r'ultralytics/cfg/models/11/yolo11-P234-deeper-FASFFHead_Jack.yaml')#使用这个结构,用大的版本就改字母yolo11l-P2-FASFFHead.yaml# 续训yaml文件的地方改为lats.pt的地址,需要注意的是如果你设置训练200轮次模型训练了200轮次是没有办法进行续训的.
-
+    model_info(model.model, verbose=True, imgsz=640)#给自定义卷积模块增加打印GFLOPS的功能，需要这个输出
     # 如何切换模型版本, 上面的ymal文件可以改为 yolov11s.yaml就是使用的v11s,
     # 类似某个改进的yaml文件名称为yolov11-XXX.yaml那么如果想使用其它版本就把上面的名称改为yolov11l-XXX.yaml即可（改的是上面YOLO中间的名字不是配置文件的）！
     # model.load('yolov11n.pt') # 是否加载预训练权重,科研不建议大家加载否则很难提升精度
